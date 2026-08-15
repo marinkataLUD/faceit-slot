5b09756c497ec495ae94ce5c2397ee28b70ea5c5
marinkataLUD <36892876+marinkataLUD@users.noreply.github.com>
2026-08-15 18:01:05 +0300

    Create PremiumClient.tsx

---
+++ app/premium/PremiumClient.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function PremiumClient() {
+  return (
+    <main className="page">
+      <section className="card">
+        <h1>FACEIT Premium with Kera4a</h1>
+        <p className="subtitle">
+          Premium FACEIT игри
+        </p>
+      </section>
+    </main>
+  );
+}
